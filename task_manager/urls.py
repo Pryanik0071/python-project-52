351f77db703525d6b7b0c9fdc9c229cb9fbba840
Dmitry M. <di.mesler.2010@yandex.ru>
2025-02-20 20:36:21 +0300

    Feat: Add start page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task_manager.views import IndexView
+
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),  # <- добавляем эту строчку
     path('admin/', admin.site.urls),
 ]
