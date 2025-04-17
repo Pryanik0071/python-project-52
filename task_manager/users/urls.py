60b66332b95ed05aeeb9c60e20e343ed7ce43cb8
Dmitry M. <di.mesler.2010@yandex.ru>
2025-04-17 22:24:49 +0300

    Refactoring: Update: Users

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -21,5 +21,6 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='users'),
-    path('create/', views.UserCreateView.as_view(), name='user_create')
+    path('create/', views.UserCreateView.as_view(), name='user_create'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -25,3 +25,24 @@ class UserCreateView(View):
         if form.is_valid():
             form.save()
         return redirect('/')
+
+
+class UserUpdateView(View):
+
+    def get(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = User.objects.get(id=user_id)
+        form = UserForm(instance=user)
+        return render(request, 'users/update.html', {'form': form, 'user_id': user_id})
+
+    # def post(self, request, *args, **kwargs):
+    #     form = UserForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #     return redirect('/')
+
+    # def get(self, request, *args, **kwargs):
+    #     category_id = kwargs.get('id')
+    #     category = Category.objects.get(id=category_id)
+    #     form = CategoryForm(instance=category)
+    #     return render(request, 'categories/update.html', {'form': form, 'category_id': category_id})
