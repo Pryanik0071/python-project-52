7edfa2cfd6fdebf4890bc973078e991530ae924d
Dmitry <di.mesler.2010@yandex.ru>
2025-04-20 16:24:02 +0300

    Feat: Add: Statuses: LC_MESSAGES

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name=_("Name"))
     inserted_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
