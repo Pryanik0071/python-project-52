569385b92b732ebe233c3ef1417dfdbe6e19155f
Dmitry <di.mesler.2010@yandex.ru>
2025-04-28 21:33:25 +0300

    Refactoring: User form

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 
 class UserForm(UserCreationForm):
-    class Meta(UserCreationForm):
+
+    first_name = forms.CharField(
+        max_length=150, required=True, label=_("First name")
+    )
+    last_name = forms.CharField(
+        max_length=150, required=True, label=_("Last name")
+    )
+
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
+        fields = ('first_name', 'last_name',
+                  'username', 'password1', 'password2'
+                  )
 
 
 class LoginForm(forms.Form):
