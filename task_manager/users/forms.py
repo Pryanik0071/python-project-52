fe4bcb1198a49b0717485e870b4fc9935e0f600d
Dmitry <di.mesler.2010@yandex.ru>
2025-04-28 22:58:13 +0300

    Update: User form - add clean username

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class UserForm(UserCreationForm):
@@ -18,6 +19,13 @@ class UserForm(UserCreationForm):
         fields = ('first_name', 'last_name',
                   'username', 'password1', 'password2'
                   )
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        # Check if the username already exists, excluding the current user
+        if User.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
+            raise ValidationError("This username is already taken.")
+        return username
 
 
 class LoginForm(forms.Form):
