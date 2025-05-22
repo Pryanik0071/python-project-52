267e726675fbf0d6e6ef611964e8779daa41600f
Dmitry <di.mesler.2010@yandex.ru>
2025-05-22 13:51:38 +0300

    Fix

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ uv pip install -r pyproject.toml
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
 .venv/bin/python manage.py migrate --noinput
-.venv/bin/python manage.py collectstatic --noinput
