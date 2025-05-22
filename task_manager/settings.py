0b0f7a8191f1886f189c81090392e9aef214666e
Dmitry <di.mesler.2010@yandex.ru>
2025-05-22 12:32:57 +0300

    Refactoring: Add postgres db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,7 +21,6 @@ load_dotenv()  # Загрузка переменных окружения из �
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -93,12 +92,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": os.getenv("DEBUG", False),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
