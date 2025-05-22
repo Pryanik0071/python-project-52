04937dcc70593b0aa8dd63221686a99b04cde078
Dmitry <di.mesler.2010@yandex.ru>
2025-05-22 13:38:29 +0300

    Fix

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv venv .venv
+source ~/.local/bin/env
+uv pip install -r pyproject.toml
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
-make install && make migrate
+.venv/bin/python manage.py migrate --noinput
+.venv/bin/python manage.py collectstatic --noinput
