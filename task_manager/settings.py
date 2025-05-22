19b0a7c26f65f677a9d3225f3b414d1457a45c93
Dmitry <di.mesler.2010@yandex.ru>
2025-05-22 13:08:01 +0300

    Fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     # Third party apps
     'django_bootstrap5',
     'django_filters',
+    'whitenoise.runserver_nostatic',
 
     # Project apps
     'task_manager',
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
@@ -144,6 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
