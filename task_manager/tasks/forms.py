562bde194e03dd7d43633081173fc68c41f1f1a5
Dmitry <di.mesler.2010@yandex.ru>
2025-05-22 19:14:58 +0300

    Refactoring: Users ordering for create tasks

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import Task
 
@@ -13,3 +14,8 @@ class TaskForm(forms.ModelForm):
             'executor',
             'labels'
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Отсортировать пользователей по id
+        self.fields['executor'].queryset = User.objects.all().order_by('id')
